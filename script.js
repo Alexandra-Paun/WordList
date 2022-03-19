2c90681b4f1bbc59ae9d654b67b6e7ad6917d8d6
Alexandra-Paun <101888869+Alexandra-Paun@users.noreply.github.com>
2022-03-19 15:25:39 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+var words = new Array();
+
+function dictionaryWords() {
+   let newWord = document.getElementById("idWord").value;
+    if (words.includes(newWord)) {
+        idParagraph.textContent = "This word is in the dictionary";
+    } else {
+        words.push(document.getElementById("idWord").value);
+        idParagraph.textContent = "";
+    }
+}
