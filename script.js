e4c199d59122468055ff10048c62b5b8ea7a895f
Alexandra-Paun <101888869+Alexandra-Paun@users.noreply.github.com>
2022-03-21 12:05:16 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-var words = new Array();
+let words = new Array();
 
 function dictionaryWords() {
    let newWord = document.getElementById("idWord").value;
-    if (words.includes(newWord)) {
-        idParagraph.textContent = "This word is in the dictionary";
-    } else {
-        words.push(document.getElementById("idWord").value);
-        idParagraph.textContent = "";
-    }
+   if (words.includes(newWord)) {
+      idParagraph.textContent = "This word is in the dictionary";
+   } else {
+      words.push(document.getElementById("idWord").value);
+      idParagraph.textContent = "";
+   }
 }
